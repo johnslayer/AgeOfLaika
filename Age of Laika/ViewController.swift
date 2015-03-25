60bc8fcfba0b6a1aaf60ae1d2d504ef99ecc900a
Johnny Awesome <johnslayer1981@gmail.com>
2015-03-25 13:11:37 -0700

    Intial Commit

--- Age of Laika/ViewController.swift
+++ Age of Laika/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelLaikasAgeInDogYears: UILabel!
+    @IBOutlet weak var textLaikasAgeInHumanYears: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonConvertToDogYears(sender: AnyObject) {
+        //first thing we should do is convert the string to an integer
+//        let stringFromTextField = textLaikasAgeInHumanYears.text
+//        let optionalIntFromTextField = stringFromTextField.toInt()
+//        let intFromOptional = optionalIntFromTextField!
+        
+        let intFromOptional = textLaikasAgeInHumanYears.text.toInt()!
+        
+        
+        //now we display it
+        labelLaikasAgeInDogYears.hidden = false
+        labelLaikasAgeInDogYears.text = "\(intFromOptional * 7)"
+        
+        //some cleanup
+        textLaikasAgeInHumanYears.text = ""
+        textLaikasAgeInHumanYears.resignFirstResponder()
+        
+        
+    }
 
+    @IBAction func convertRealConversionToDogYearsPressed(sender: AnyObject) {
+        let stringFromTextField = textLaikasAgeInHumanYears.text
+        let optionalIntFromTextField = stringFromTextField.toInt()
+        var intFromOptional = optionalIntFromTextField!
+        
+        if intFromOptional <= 2 {
+            labelLaikasAgeInDogYears.hidden = false
+            var doubleFromOptional = Double(intFromOptional)
+            labelLaikasAgeInDogYears.text = "\(doubleFromOptional * 10.5)"
+        }
+        else {
+            labelLaikasAgeInDogYears.hidden = false
+            labelLaikasAgeInDogYears.text = "\(intFromOptional * 4)"
+        }
+     
+        textLaikasAgeInHumanYears.text = ""
+        textLaikasAgeInHumanYears.resignFirstResponder()
+
+    }
 }
 
